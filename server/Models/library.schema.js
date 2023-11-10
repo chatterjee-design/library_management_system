503ee3f5329c76cd60cf8efa430eef965baeef8d
Mousumi Chatterjee <mousumichatterjee6379@gmail.com>
2023-11-10 15:16:23 +0530

    implement data model for library

---
+++ server/Models/library.schema.js
@@ -0,0 +1,40 @@
+import {model, Schema } from "mongoose";
+
+const librarySchema = new Schema({
+    booksName: {
+        type : String,
+        required : [true, "Name of the book is required"],
+        minlength : [4, "Name must be at least 4 characters"],
+        maxlength : [25, "Name cant not exceed 25 characters"],
+        trim : true,
+    },
+    description: {
+        type : String,
+        required : [true, "Description of the book is required"],
+        minlength : [8, "Description must be at least 4 characters"],
+        maxlength : [55, "Description cant not exceed 25 characters"],
+        trim : true,
+    },
+    writer : {
+        type : String,
+        required : [true, "writer of the book is required"],
+        trim : true
+    },
+    numberOfBooks : {
+        type : String,
+        required : [true, "writer of the book is required"],
+        trim : true
+    },
+    thumbnail : {
+        public_id: {
+            type: String,
+          },
+          secure_url: {
+            type: String,
+          },
+    }
+})
+
+const library = model('library', librarySchema)
+
+export default library;
