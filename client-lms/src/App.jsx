c55a978137037f463041d533b05822f8956761c4
Mousumi Chatterjee <mousumichatterjee6379@gmail.com>
2023-11-17 12:52:47 +0530

    added login page

--- client-lms/src/App.jsx
+++ client-lms/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import ErrorPage from './Pages/ErrorPage';
 import HomePage from './Pages/HomePage';
+import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
     
-      <SignUp/>
+      <Login/>
     </>
   )
 }
---
+++ client-lms/src/Pages/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+     <div className="flex items-center h-screen w-screen justify-center bg-login bg-cover bg-no-repeat ">
+        <div className=" backdrop-blur-sm h-screen w-screen flex items-center justify-center">
+          <div className="flex flex-col items-center justify-between py-5 min-h-fit h-[65%] bg-white w-[80%] md:w-2/5">
+            <h1 className="uppercase font-light tracking-[.35em] sm:text-3xl font-serif text-[#269d8b]" >LogIn</h1>
+            <form className=" flex flex-col items-center justify-evenly w-[80%] h-[100%] ">
+
+              <div className="flex flex-col w-[100%] justify-center gap-3">
+                <label className="font-mono tracking-[0.15em]" htmlFor="email">Email:</label>
+                <input placeholder="Enter Your email" className="border-b" type="email" id="email" name="email" required />
+              </div>
+
+              <div className="flex flex-col w-[100%] justify-center gap-3">
+                <label className="font-mono tracking-[0.15em]" htmlFor="password">Password:</label>
+                <input placeholder="Enter Your Password" className="border-b" type="password" id="password" name="password" required />
+              </div>
+
+              <button className="btn bg-[#5c269d] text-white tracking-[0.3em] btn-primary" type="submit">Submit</button>
+            </form>
+            <p>Don't have an account? <span className=" text-[#5c269d] pt-4 cursor-pointer hover:text-[#796b87] underline font-sans">Sign Up</span> </p>
+          </div>
+        </div>
+      </div> 
+    </>
+  )
+}
+
+export default Login
