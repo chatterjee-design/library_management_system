5d3b1504bf7976c4735431023b6a0dea763b00eb
Mousumi Chatterjee <mousumichatterjee6379@gmail.com>
2023-11-20 15:57:24 +0530

    update is logged in auth

--- client-lms/src/Components/Auth/IsLoggedInAuth.jsx
+++ client-lms/src/Components/Auth/IsLoggedInAuth.jsx
@@ -1,9 +1,22 @@
+import toast from "react-hot-toast";
 import { useSelector } from "react-redux"
 import { Navigate, Outlet } from "react-router-dom";
 
 const IsLoggedInAuth = ({children}) =>{
     const {isLoggedIn} = useSelector((state)=> state.auth);
-    return isLoggedIn ? <>{children}</> : <Navigate to="/logIn" />;
+    if (!isLoggedIn) {
+        // Show a toast to notify the user
+
+        toast('Please Login First', {
+            icon: '😊',
+          });
+
+        // Redirect to login page
+        return <Navigate to="/logIn" />;
+      }
+    
+      return <>{children}</>;
+    
 }
 
 export default IsLoggedInAuth
