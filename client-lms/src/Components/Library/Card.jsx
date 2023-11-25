e6c7cb6d2cd752221e1e94cc2dafd4fe237df6c3
Mousumi Chatterjee <mousumichatterjee6379@gmail.com>
2023-11-25 13:09:43 +0530

    change the card ui

--- client-lms/src/Components/Library/Card.jsx
+++ client-lms/src/Components/Library/Card.jsx
@@ -6,13 +6,16 @@ const Card = ({ data }) => {
 
   return (
     <>
-      <div className="card card-compact cursor-pointer w-80 bg-base-100 shadow-xl opacity-0 animate-slide-in-left">
+      <div className="card card-compact cursor-pointer pt-7 w-72 bg-base-100 shadow-sd opacity-0 animate-slide-in-left">
         <figure>
-          <img src={data?.thumbnail?.secure_url} alt="Shoes" />
+          <img src={data?.thumbnail?.secure_url} className=" w-48 h-72 " alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title tracking-[.24em] font-light  font-serif">
+          <h2 className="card-title tracking-[.24em] font-light text-sm font-serif">
             {data?.bookName}
+          </h2>
+          <h2 className="card-title tracking-[.24em] font-light text-xs text-gray-400 font-mono">
+            {data?.writer}
           </h2>
           <div className="card-actions justify-end">
             <Link to={`/library/books/${_id}`} className="btn bg-[#5c269d] text-white tracking-[0.4em] btn-primary">
